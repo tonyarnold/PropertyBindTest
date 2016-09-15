7549ea4c856889f0b33c7c0faf7d0d273335165c
Tony Arnold <tony@thecocoabots.com>
2016-09-15 10:30:28 +1000

    Initial Commit

---
+++ PropertyBindTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PropertyBindTest
+//
+//  Created by Tony Arnold on 15/9/16.
+//  Copyright © 2016 The CocoaBots. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
